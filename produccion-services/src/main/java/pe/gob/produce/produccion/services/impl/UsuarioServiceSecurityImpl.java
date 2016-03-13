ff260aa8bbf446cfa9112ecc11ee4ae5f3e9ac3e
jearph <jearph.ing@gmail.com>
2016-03-13 18:14:14 -0500

    Updated with Spring Security

---
+++ produccion-services/src/main/java/pe/gob/produce/produccion/services/impl/UsuarioServiceSecurityImpl.java
@@ -0,0 +1,53 @@
+package pe.gob.produce.produccion.services.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import pe.gob.produce.produccion.bo.UsuarioBO;
+import pe.gob.produce.produccion.services.UsuarioServiceSecurity;
+import pe.gob.produce.produccion.services.UsuarioServices;
+
+@SuppressWarnings("deprecation")
+@Service("usuarioServiceSecurity")
+@Transactional(value="transactionManagerSeguridad")
+public class UsuarioServiceSecurityImpl implements UsuarioServiceSecurity {
+
+	@Autowired
+	private UsuarioServices usuarioService;
+	
+	@Override
+	public UserDetails loadUserByUsername(String usuario)
+			throws UsernameNotFoundException {
+		UsuarioBO usuarioBO = null;
+		try {
+			usuarioBO = usuarioService.obtenerUsuario(usuario);
+			if(usuario==null){
+				throw new UsernameNotFoundException("Usuario no encontrado");
+			}
+		} catch (Exception e) {
+			throw new UsernameNotFoundException(e.getMessage()); 
+		}
+		return  makeUser(usuarioBO);
+	}
+
+	private User makeUser(UsuarioBO usuario){
+		return new User(usuario.getIdUsuario(), usuario.getContrasenia(),
+                true, true, true, true, makeGrantedAuthorities(usuario));
+	}
+
+	private Collection<GrantedAuthority> makeGrantedAuthorities(UsuarioBO usuario){
+		Collection<GrantedAuthority> result = new ArrayList<GrantedAuthority>();
+		result.add(new GrantedAuthorityImpl("ROL1"));
+        return result;
+	}
+	
+}
